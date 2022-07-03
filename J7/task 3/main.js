91169f38de96936ad41afd4e4fa6de65046dc5fe
Anna <sugajloana@gmail.com>
2022-07-03 21:40:50 +0300

    J7

--- J7/task 3/main.js
+++ J7/task 3/main.js
@@ -5,3 +5,43 @@
 //
 // randUser('male') //{name:"Ivan", age : 77, gender:"male"}
 // randUser('female') //{name:"Inna", age : 3, gender:"female"}
+function getRandomUsers(gender) {
+    let usersMale = ['Ivan', 'Taras', 'Stanislav', 'Bohdan', 'Maksim',
+        'Nazar', 'Dmitriy', 'Roman', 'Vova', 'Oleg',
+        'Vitaliy', 'Oleksandr', 'Viktor', 'Sergiy', 'Ostap',
+        'Yuri', 'Evgeniy', 'Egor', 'Platon', 'Anatoliy',
+        'Vladislav', 'Andriy', 'Igor', 'Mukola', 'Zahar',
+        'Artem', 'Denis', 'Timofey', 'Gleb', 'Eduard',
+        'Ilya', 'Nikita', 'Kirill', 'Artur', 'Vadim',
+        'Arsen', 'Mykhailo', 'Pavlo', 'Petro', 'Timur',
+        'David', 'Mark', 'Anton', 'Danilo', 'Adam',
+        'Boris', 'Valentin', 'Vasyl', 'Gennady', 'Hieorhij'];
+
+    let usersFemale = ['Anna', 'Olena', 'Natalia', 'Valentina', 'Galina',
+        'Tetiana', 'Alina', 'Alla', 'Anastasia', 'Oksana',
+        'Larisa', 'Julia', 'Lidia', 'Ludmila', 'Irina',
+        'Olga', 'Katerina', 'Sofia', 'Oleksandra', 'Uliana',
+        'Darina', 'Diana', 'Maria', 'Viktoria', 'Kristina',
+        'Elizaveta', 'Angelina', 'Veronika', 'Polina', 'Yana',
+        'Karina', 'Valeria', 'Eva', 'Arina', 'Stefania',
+        'Alisa', 'Antonina', 'Vita', 'Vira', 'Vitalia',
+        'Edita', 'Eleonora', 'Emma', 'Evgenia', 'Zoja',
+        'Inna', 'Karolina', 'Klavdia', 'Lilia', 'Lyubov'];
+
+    const result = {name: '', age: 0, gender: gender};
+
+    result.age = Math.floor(Math.random() * 1000) % 100;
+
+    if (gender === 'male') {
+        result.name = usersMale[Math.floor(Math.random() * 1000) % 50];
+    } else if (gender === 'female') {
+        result.name = usersFemale[Math.floor(Math.random() * 1000) % 50];
+    } else {
+        return "Error";
+    }
+
+    return result;
+}
+
+console.log(getRandomUsers('male'))
+console.log(getRandomUsers('female'))
